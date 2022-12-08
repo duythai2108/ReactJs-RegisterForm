7c3b71a84baae4d68e26bc58b4145ada36d25193
duythai2108 <thaiduy21082000@gmail.com>
2022-12-08 11:43:39 +0700

    ReactJS RegisterForm using ReactHookForm

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import RegisterHook from "./components/form/RegisterHook";
+// import RegisterFormik from "./components/form/RegisterFormik";
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterHook></RegisterHook>
+    </div>
+  );
+}
+
+export default App;

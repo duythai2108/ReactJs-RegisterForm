4f5b96a0e11f903c306998a7a13f0b98cbdfaf1f
duythai2108 <thaiduy21082000@gmail.com>
2022-12-13 18:07:54 +0700

    Finish Register using Formik

--- src/components/form/RegisterFormik.jsx
+++ src/components/form/RegisterFormik.jsx
@@ -2,7 +2,31 @@ import React from "react";
 import InputFormik from "../input/InputFormik";
 import { Formik, Form, Field, ErrorMessage, useField } from "formik";
 import * as yup from "yup";
-
+import RadioFormik from "../radio/RadioFormik";
+import CheckboxFormik from "../checkbox/CheckboxFormik";
+import DropdownFormik from "../dropdown/DropdownFormik";
+const dropdownData = [
+  {
+    id: 1,
+    value: "teacher",
+    text: "Teacher",
+  },
+  {
+    id: 2,
+    value: "developer",
+    text: "Developer",
+  },
+  {
+    id: 3,
+    value: "doctor",
+    text: "Doctor",
+  },
+  {
+    id: 4,
+    value: "constructor",
+    text: "Constructor",
+  },
+];
 const RegisterFormik = () => {
   return (
     <Formik
@@ -39,27 +63,87 @@ const RegisterFormik = () => {
           .string()
           .required("Please select your job")
           .oneOf(["teacher", "developer", "doctor", "constructor"]),
-        term: yup.boolean().required("Please accept the terms and conditions"),
+        term: yup
+          .boolean()
+          .oneOf([true], "Please accept the terms and conditions"),
       })}
-      
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        setTimeout(() => {
+          console.log(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+          resetForm();
+        }, 5000);
+      }}
     >
-      <Form className="max-w-[300px] mx-auto my-10" autoComplete="off">
-        <div className="flex flex-col gap-3 mb-5">
-          <label htmlFor="username" className="cursor-pointer">
-            Username
-          </label>
-          <InputFormik
-            name="username"
-            placeholder="Enter your username"
-            id="username"
-            type="text"
-          ></InputFormik>
-          <p className="text-sm text-red-500">Please enter your username</p>
-        </div>
-        <button className="w-full p-5 mt-5 font-semibold text-white bg-blue-500 rounded-lg">
-          Submit
-        </button>
-      </Form>
+      {(formik) => {
+        const watchGender = formik.values.gender;
+        return (
+          <form
+            onSubmit={formik.handleSubmit}
+            className="max-w-[300px] mx-auto my-10"
+            autoComplete="off"
+          >
+            <InputFormik
+              name="username"
+              placeholder="Enter your username"
+              id="username"
+              label="Username"
+              type="text"
+            ></InputFormik>
+            <InputFormik
+              name="email"
+              placeholder="Enter your email"
+              id="email"
+              label="Email address"
+              type="email"
+            ></InputFormik>
+            <InputFormik
+              name="password"
+              placeholder="Enter your password"
+              id="password"
+              label="Password"
+              type="password"
+            ></InputFormik>
+            <div className="flex flex-col gap-3 mb-5">
+              <label className="cursor-pointer">Gender</label>
+              <div className="flex items-center gap-5">
+                <RadioFormik
+                  name="gender"
+                  value="male"
+                  checked={watchGender === "male"}
+                  label="Male"
+                ></RadioFormik>
+                <RadioFormik
+                  name="gender"
+                  value="female"
+                  checked={watchGender === "female"}
+                  label="Female"
+                ></RadioFormik>
+              </div>
+            </div>
+            <DropdownFormik
+              labeltext="Your job"
+              data={dropdownData}
+              name="job"
+              setValue={formik.setFieldValue}
+            ></DropdownFormik>
+            <CheckboxFormik name="term">
+              I accept the terms and conditions
+            </CheckboxFormik>
+            <button
+              type="submit"
+              disabled={formik.isSubmitting}
+              className="w-full p-5 mt-5 font-semibold text-white bg-blue-500 rounded-lg"
+            >
+              {formik.isSubmitting ? (
+                <div className="w-5 h-5 mx-auto border-2 border-t-2 border-white rounded-full border-t-transparent animate-spin"></div>
+              ) : (
+                "Submit"
+              )}
+            </button>
+          </form>
+        );
+      }}
     </Formik>
   );
 };
--- src/components/input/InputFormik.jsx
+++ src/components/input/InputFormik.jsx
@@ -1,11 +1,23 @@
+import { useField } from "formik";
 import React from "react";
 
-const InputFormik = () => {
+const InputFormik = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
   return (
-    <input
-      className="p-4 border border-gray-100 rounded-lg bg-white outline-none
+    <div className="flex flex-col gap-3 mb-5">
+      <label htmlFor={props.id} className="cursor-pointer">
+        {label}
+      </label>
+      <input
+        className="p-4 border border-gray-100 rounded-lg bg-white outline-none
       transition-all focus:border-blue-500"
-    ></input>
+        {...field}
+        {...props}
+      ></input>
+      {meta.touched && meta.error && (
+        <p className="text-sm text-red-500">{meta.error}</p>
+      )}
+    </div>
   );
 };
 
